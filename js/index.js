392d280adbd5b7d70b9bfd7cc6838ac65387e53b
TonyDcx <81106336+TonyDcx@users.noreply.github.com>
2021-05-14 11:56:17 +0300

    E-Wallet
    Added How It Works Card

--- js/index.js
+++ js/index.js
@@ -4,14 +4,12 @@
 
 // * Social Media Alert 
 
-let closeBtn = document.getElementById('close-social');
+let closeButton = document.getElementById('close-social');
 
-let collapseSocial = document.getElementById('alert-social');
+closeButton.addEventListener('click' , function(addOff) {
+    let socialMedia = document.getElementById('alert-social');
 
-closeBtn.addEventListener('click', function(collapseSocial){
- 
-    document.getElementById('alert-social').classList.add('off');
-
+    socialMedia.classList.add('off');
 });
 
 // * Info Container Animation Added 
@@ -23,3 +21,7 @@ document.getElementsByClassName('show-active')[0].style.display = "none";
 showMore.addEventListener('click' , function(show){
     document.getElementsByClassName('show-active')[0].style.display = "block";
 });
+   
+
+// * 
+
